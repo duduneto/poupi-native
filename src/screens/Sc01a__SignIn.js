ddaeef9f5b36515a07a37fb918bccc372db57030
JuanDSG <juandiegosg@gmail.com>
2020-01-07 13:04:43 -0400

    chore: simple position code

--- src/screens/Sc01a__SignIn.js
+++ src/screens/Sc01a__SignIn.js
@@ -3,11 +3,11 @@
 // ---------------
 
 // import Packages
-import React from 'react';
+import React from "react";
 
 // import Internals
-import * as AllSc from './';
-import { useHistory, useRd, useChangeRd, useForm } from '../useMorfos';
+import * as AllSc from "./";
+import { useHistory, useRd, useChangeRd, useForm } from "../useMorfos";
 
 // ---------------
 // #endregion
@@ -22,10 +22,10 @@ export default function Sc00() {
   let { rdContent, rdAuthUser, rdIptsSignUp } = useRd();
   let history = useHistory();
   let changeRd = useChangeRd();
-  let scContent = rdContent.sc01a 
   // let { inputChange } = useForm('rdIptsSignUp');
 
-  let redirect = rdAuthUser && history.push('/profile');
+  let redirect = rdAuthUser && history.push("/profile");
+  let scContent = rdContent.sc01a;
 
   // ---------------
   // #endregion
@@ -38,16 +38,16 @@ export default function Sc00() {
   let signIn = () => {
     // set Call
     let infoUser = {
-      userName: 'Maria',
-      email: 'ladymary@email.com',
+      userName: "Maria",
+      email: "ladymary@email.com",
       age: 32
     };
 
     // call Hook
-    changeRd('rdAuthUser', infoUser);
+    changeRd("rdAuthUser", infoUser);
 
     // redirect
-    history.push('/profile');
+    history.push("/profile");
   };
 
   // let start = () => {}
@@ -63,8 +63,8 @@ export default function Sc00() {
   // let model = () =>
 
   let enter = () => signIn();
-  let toTerms = () => history.push('/terms');
-  let toTempUsers = () => history.push('/temp-users');
+  let toTerms = () => history.push("/terms");
+  let toTempUsers = () => history.push("/temp-users");
 
   // ---------------
   // #endregion
