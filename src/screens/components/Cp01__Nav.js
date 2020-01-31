2614c4bf802ae6b8b1ba8e2a8ce36c083bf15150
solrack02 <carlosvsk@gmail.com>
2020-01-31 10:44:22 -0300

    bug: goBack + add: back prop

--- src/screens/components/Cp01__Nav.js
+++ src/screens/components/Cp01__Nav.js
@@ -46,61 +46,54 @@ export default function Sc00(props) {
   let Nav = {
     "/temp-users": {
       title: scContent.sc00b,
-      menu: false,
+      back: "/sign-in",
       longBar: false
     },
     "/terms": {
       title: scContent.sc01b,
-      menu: false,
+      back: "/sign-in",
       longBar: true
     },
     "/category-products": {
       title: scContent.sc02a,
-      menu: true,
       longBar: false
     },
     "/products": {
       title: scContent.sc02b,
-      menu: false,
+      back: "/category-products",
       longBar: false
     },
     "/stores": {
       title: scContent.sc02c,
-      menu: true,
       longBar: false
     },
     "/profile": {
       title: scContent.sc03a,
-      menu: true,
       longBar: true
     },
     "/profile-product": {
       title: scContent.sc03b,
-      menu: false,
+      back: "/products",
       longBar: true
     },
     "/profile-store": {
       title: scContent.sc03c,
-      menu: false,
+      back: "/stores",
       longBar: true
     },
     "/add-products": {
       title: scContent.sc04a,
-      menu: false,
+      back: "/category-products",
       longBar: true
     }
   };
 
   let CompNav = Nav[props.path];
 
-  let icon = CompNav.menu ? "menu" : "left";
+  let icon = !CompNav.back ? "menu" : "left";
 
   let condGoTo = () =>
-    CompNav.menu
-      ? CompNav.menu
-        ? toggleMenu()
-        : history.push(CompNav.menu)
-      : history.goBack();
+    !CompNav.back ? toggleMenu() : history.push(CompNav.back);
 
   let toggleMenu = () => setMenu(!sttMenu);
   let condMenu = sttMenu;
