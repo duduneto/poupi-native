a85a8738e1600dad9b73eeff47f4223d3f9e998f
solrack02 <solrack02>
2020-04-16 15:35:37 -0300

    change: app name

--- ios/myappTests/myappTests.m
+++ ios/myappTests/myappTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface myappTests : XCTestCase
+@interface start_morfosTests : XCTestCase
 
 @end
 
-@implementation myappTests
+@implementation start_morfosTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
