d0f8a6aef3b70b3db03857ad4cae767653f2e6b4
solrack02 <solrack02>
2020-08-12 14:49:39 -0300

    bug: conflict utils

--- src/useMorfos/utils.js
+++ src/useMorfos/utils.js
@@ -15,4 +15,61 @@ export default {
     for (var item in obj) newArr.push(obj[item]);
     return newArr;
   },
+
+  setPath: (obj, path = '') =>
+    path.split('.').reduce((p, c) => p && p[c], obj && obj),
+
+  hasData(data) {
+    const checkArr = [];
+
+    const loop = info => {
+      const isObj = typeof info === 'object';
+      if (isObj) {
+        for (var item in info) loop(info[item]);
+        return;
+      }
+      checkArr.push(info && true);
+    };
+
+    loop(data);
+
+    const findTrue = checkArr.find(item => item);
+    const finalCheck = findTrue ? true : false;
+    return finalCheck;
+  },
+
+  mergeDeep: (...objects) => {
+    const isObject = obj => obj && typeof obj === 'object';
+
+    return objects.reduce((prev, obj) => {
+      Object.keys(obj).forEach(key => {
+        const pVal = prev[key];
+        const oVal = obj[key];
+
+        if (Array.isArray(pVal) && Array.isArray(oVal)) {
+          prev[key] = pVal.concat(...oVal);
+        } else if (isObject(pVal) && isObject(oVal)) {
+          prev[key] = utils.mergeDeep(pVal, oVal);
+        } else {
+          prev[key] = oVal;
+        }
+      });
+
+      return prev;
+    }, {});
+  },
+
+  currencyMask: (info, returnAsNumber) => {
+    if (returnAsNumber) {
+      return Number(String(info).replace(/\D/g, '')) / 100;
+    } else {
+      const _info = String(info);
+      const onlyNumbers = Number(_info.replace(/\D/g, ''));
+      const stringWithMask = (onlyNumbers / 100)
+        .toFixed(2)
+        .replace('.', ',')
+        .replace(/\d(?=(\d{3})+\,)/g, '$&.');
+      return stringWithMask;
+    }
+  },
 };
