73e5ef95621c22aff9dc6eaacf5b478b87bce726
Juan Diego <juandiegosg@gmail.com>
2020-08-12 13:45:24 -0300

    add: toArr utils

--- src/useMorfos/utils.js
+++ src/useMorfos/utils.js
@@ -9,4 +9,10 @@ export default {
   filterFn: (arr, type) => arr.filter(res => res.type === type),
 
   timeFunc: func => setTimeout(func, 900),
+
+  toArr(obj) {
+    var newArr = [];
+    for (var item in obj) newArr.push(obj[item]);
+    return newArr;
+  },
 };
