40e0aa7683492963a1b04c376b08ce13ec4c8345
solrack02 <solrack02>
2020-08-13 16:05:49 -0300

    add: loader flex

--- src/useMorfos/UseLoader.js
+++ src/useMorfos/UseLoader.js
@@ -1,19 +1,25 @@
 // ----------- import Packs
 import React from 'react';
-import { ActivityIndicator } from 'react-native';
+import { ActivityIndicator, View } from 'react-native';
 
 // ----------- import Internals
 import { primaryColor } from '../config/styles';
+import useStl from './useStl';
+
+// ----------- set Styles
+const stlCenter = [useStl.flexCenter, useStl.flex1];
 
 // ----------- set Default Component
 export default props => {
   const condSize = props.size ?? 15;
   return (
-    <ActivityIndicator
-      // style={props.style}
-      size={condSize}
-      {...props}
-      color={props.color || primaryColor}
-    />
+    <View style={stlCenter}>
+      <ActivityIndicator
+        // style={props.style}
+        size={condSize}
+        {...props}
+        color={props.color || primaryColor}
+      />
+    </View>
   );
 };
