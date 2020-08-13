5a059b29170aa74fa7840206e707435978dfea63
Juan Diego <juandiegosg@gmail.com>
2020-08-13 14:40:21 -0300

    add: useStl pad

--- src/useMorfos/useStl.js
+++ src/useMorfos/useStl.js
@@ -1,2 +1,26 @@
 import useStl from '../config/styles';
-export default useStl;
+import utils from './utils';
+
+const useStl = {
+  pad: (top, right, botton, left) => {
+    if ((right === undefined, botton === undefined, left === undefined)) {
+      return {
+        paddingTop: top,
+        paddingRight: top,
+        paddingLeft: top,
+        paddingBottom: top,
+      };
+    }
+
+    return {
+      paddingTop: top,
+      paddingRight: right,
+      paddingLeft: bottom,
+      paddingBottom: left,
+    };
+  },
+
+  undefined: useStl,
+};
+
+export default utils;
