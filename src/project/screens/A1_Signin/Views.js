dc5f5f49e39c27f8278121596c32337b2c8a0bf7
solrack02 <solrack02>
2020-08-13 11:41:48 -0300

    change: A1 test

--- src/project/screens/A1_Signin/Views.js
+++ src/project/screens/A1_Signin/Views.js
@@ -1,6 +1,12 @@
 // ----------- import Packs
 import React from 'react';
-import { View, Text } from 'react-native';
+import {
+  View,
+  Text,
+  ImageBackground,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
 import { useSelector } from 'react-redux';
 
 // ----------- import Internals
@@ -9,11 +15,13 @@ import { Counter1, Btn1 } from '../../comps';
 
 // #region :: STYLEs *********
 const stlBodyView = [
-  useStl.flex1,
-  useStl.pad20,
-  useStl.bgSc,
-  useStl.flexTopCenter,
+  // useStl.flex1,
+  // useStl.pad20,
+  // useStl.bgSc,
+  // useStl.flexTopCenter,
+  { backgroundColor: 'red' },
 ];
+const stlBanner = [useStl.flexCenter, { height: 500 }];
 const stlTitleTxt = [useStl.txtTitleScreen];
 const stlSubTitleTxt = [useStl.txtSubTitleCard];
 const stlDescView = [{ width: 200 }];
@@ -25,6 +33,29 @@ export default ({ info }) => {
   return (
     <View style={stlBodyView}>
       {/* ----------- PARTs */}
+      <ImageBackground source={info.background} style={stlBanner} />
+      {/* 
+        <Image source={info.curves} style={stlImgCurves} />
+        <Image source={info.detail} style={stlImgDetail} />
+        <Image source={info.sport} style={stlImgSport} />
+
+        <View style={stl03}>
+          <Image source={info.logo} style={stl01d} />
+
+          <span>{info.scContent.slogan}</span>
+          <span>
+            Suas aulas a <span>qualquer hora</span> e em
+            <span>qualquer lugar</span>
+          </span>
+
+          <Text style={stl04b}>{info.scContent.enterUsing}</Text>
+          <TouchableOpacity style={stl02} onPress={info.enter}>
+            <Text style={stl02a}>ENTRE AGORA</Text>
+            <Image source={btnBg} style={stlBgBt} />
+          </TouchableOpacity>
+        </View>
+      </ImageBackground> */}
+
       <Text style={stlTitleTxt}>{info.content.title}</Text>
 
       <Text style={stlSubTitleTxt}>{info.content.subTitle}</Text>
