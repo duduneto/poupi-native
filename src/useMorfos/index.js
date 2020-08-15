98e9a64ce477325747634480fb76c70805b3ebc3
Juan Diego <juandiegosg@gmail.com>
2020-08-15 17:00:57 -0300

    change: delete useForm

--- src/useMorfos/index.js
+++ src/useMorfos/index.js
@@ -5,7 +5,6 @@ export { default as UseLoader } from './UseLoader';
 export { default as UseIcoMoon } from './UseIcoMoon';
 export { default as UseInitData } from './UseInitData';
 
-export { default as useForm } from './useForm';
 export { default as useResize } from './useResize';
 export { default as useRouter } from './useRouter';
 export { default as useStl } from './useStl';
