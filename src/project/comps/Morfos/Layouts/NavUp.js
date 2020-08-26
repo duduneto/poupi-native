bf3dfcbf19bfb4433b9661de9c85ba4a83a654d5
solrack02 <solrack02>
2020-08-26 16:18:53 -0300

    add: SideLeft + callRouter + condColor

--- src/project/comps/Morfos/Layouts/NavUp.js
+++ src/project/comps/Morfos/Layouts/NavUp.js
@@ -128,7 +128,7 @@ const ViewDF = ({ info }) => {
   return (
     <>
       {/* MENU */}
-      <SideLeft content={info.content} />
+      <SideLeft path={'mktList'} content={info.content} />
 
       <View style={stlNAV1}>
         <ImageBackground
--- src/project/comps/Morfos/Layouts/SideLeft.js
+++ src/project/comps/Morfos/Layouts/SideLeft.js
@@ -1,16 +1,49 @@
 // ----------- import Packs
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 
 // ----------- import Internals
-import { useStl } from '../../../../useMorfos';
+import { useStl, UseIcoMoon, useRouter } from '../../../../useMorfos';
 
 // #region :: STYLEs *********
 
-const stlSideMenu = [useStl.leftBar];
+const stlSideMenu = [useStl.leftBar, useStl.pad(80, 0)];
+const stlItem = [useStl.flexRow, useStl.pad(10, 20)];
+const stlLabel = [{ paddingLeft: 6 }];
 
 // #endregion *********
 
-export default ({ children }) => {
-  return <View style={stlSideMenu}>{children}</View>;
+export default ({ children, path }) => {
+  // ---------- set Hooks
+  const { callRouter } = useRouter();
+
+  // ---------- set Itens Menu
+  const arrMenu = [
+    { icon: 'cart', label: 'Supermercados', goTo: 'mktList' },
+    { icon: 'user', label: 'Produtos', goTo: 'prodList' },
+    { icon: 'user', label: 'Sair', goTo: 'logout' },
+  ];
+
+  const ItemsList = arrMenu.map(item => {
+    const logout = item.goTo === 'logout';
+    const goTo = () => {
+      if (logout) {
+        callRouter('signin');
+      } else {
+        callRouter(item.goTo);
+      }
+    };
+
+    const condActiveMenu = item.goTo === path;
+    const condColor = condActiveMenu ? '#66cc66' : '#444';
+
+    return (
+      <TouchableOpacity style={stlItem} onPress={() => goTo()}>
+        <UseIcoMoon name={item.icon} size={20} color={condColor} />
+        <Text style={[stlLabel, { color: condColor }]}>{item.label}</Text>
+      </TouchableOpacity>
+    );
+  });
+
+  return <View style={stlSideMenu}>{ItemsList}</View>;
 };
