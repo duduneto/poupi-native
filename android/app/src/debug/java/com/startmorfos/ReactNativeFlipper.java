94adfeb5ddc9295d6a45174f1b4cc8908bddb092
solrack02 <solrack02>
2020-08-27 15:36:14 -0300

    add: new react-native

--- android/app/src/debug/java/com/startmorfos/ReactNativeFlipper.java
+++ android/app/src/debug/java/com/startmorfos/ReactNativeFlipper.java
@@ -4,7 +4,7 @@
  * <p>This source code is licensed under the MIT license found in the LICENSE file in the root
  * directory of this source tree.
  */
-package com.gym-one;
+package com.startmorfos;
 
 import android.content.Context;
 import com.facebook.flipper.android.AndroidFlipperClient;
