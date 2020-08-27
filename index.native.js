4ae0306f0a53db14f7a07088fa6af5ab8e5e49dd
solrack02 <solrack02>
2020-08-27 16:35:47 -0300

    change: android ok

--- index.native.js
+++ index.native.js
@@ -1,5 +1,9 @@
 import { AppRegistry } from 'react-native';
-import Root from './src/Root';
-import { name } from './src/appInfo';
+// import Root from './src/Root';
+import Root from './src/Duarte';
+// import { name } from './src/appInfo';
+
+// Nome Temporário
+const name = 'StartMorfos';
 
 AppRegistry.registerComponent(name, () => Root);
---
+++ src/Duarte/index.js
@@ -0,0 +1,15 @@
+// import packages
+import React from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+// import { firestore } from '../config/firebase/fbConfig';
+
+export default () => {
+  // const usersCollection = async () => await firestore.collection('Users');
+
+  // console.log('usersCollection', usersCollection);
+  return (
+    <View>
+      <Text>Android Funcionando com Firestore</Text>
+    </View>
+  );
+};
